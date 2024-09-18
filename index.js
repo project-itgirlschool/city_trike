b4ecf99920d966ff872151755c38553c2fc1251e
Taticornaz <tatyana.korotkova.1997@mail.ru>
2024-09-18 15:10:18 +0200

    added block 2

---
+++ index.js
@@ -0,0 +1,16 @@
+// const element = document.querySelector('.my-element');
+// element.classList.add('animate__animated', 'animate__backInDown');
+
+// element.addEventListener('animationend', () => {
+//     element.style.color = "#e03a3a";
+//     element.style.borderColor = "#ffff";
+// });
+
+
+
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const formContainer = document.getElementById('formContainer');
+    formContainer.classList.toggle('hidden');
+});
+
