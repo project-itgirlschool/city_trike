c3841bdd93087f6126771f9a36f99c90cdb595cb
Taticornaz <tatyana.korotkova.1997@mail.ru>
2024-09-23 13:37:30 +0200

    Added block 2

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ element.addEventListener('animationend', () => {
 
 
 const buttons = document.querySelectorAll('.toggleButton');
+const closeBtn = document.getElementById('closeBtn');
+
+closeBtn.addEventListener('click', function() {
+    const formContainer = document.getElementById('formContainer');
+    formContainer.classList.toggle('hidden-form'); 
+});
+
 buttons.forEach((button, index) => {
     button.addEventListener('click', function() {
         const formContainer = document.getElementById(`formContainer`);
@@ -28,6 +35,29 @@ buttons.forEach((button, index) => {
   
 });
 
+
+const back = document.querySelector('#back');
+const next = document.querySelector('#next');
+
+const photos = ["/assets/images/traik-photo-2.jpeg","/assets/images/traik-photo-3.jpeg"];
+
+let i = 0;
+
+next.addEventListener("click", ()=>{
+    i++;
+    if(i>photos.length - 1){
+        i = 0;
+    }
+    document.querySelector("#pictures-of-traik").src = photos[i];
+})
+
+back.addEventListener("click", ()=>{
+    i--;
+    if(i<0 ){
+        i = photos.length - 1;
+    }
+    document.querySelector("#pictures-of-traik").src = photos[i];
+})
 
 const video = document.getElementById("video-traik-block2");
 const playPauseBtn = document.getElementById("playPauseBtn");
